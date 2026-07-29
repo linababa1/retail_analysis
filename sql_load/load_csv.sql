79c357a7d422dc20e25905732b3f6faaa58680e0
lina <linababa987@gmail.com>
2026-07-29 09:09:15 +0200

    loaded csv to sql tables

--- sql_load/load_csv.sql
+++ sql_load/load_csv.sql
@@ -1,3 +1,4 @@
-SET datestyle = 'MDY';
+SET datestyle = 'DMY';
 copy sales_data FROM 'C:\Analytics Engineer\SQL PROJECTS\retail_analysis\csv_file\superstore_sale.csv' WITH (FORMAT csv, HEADER true, DELIMITER ';');
 
+SELECT * FROM sales_data LIMIT 10;
