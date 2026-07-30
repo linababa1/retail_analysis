b41ca82f779c2b9e46633e69c307533f3e26d480
lina <linababa987@gmail.com>
2026-07-30 09:34:37 +0200

    top 5 city and state with the most sales

--- sql_load/load_csv.sql
+++ sql_load/load_csv.sql
@@ -1,4 +1,4 @@
 SET datestyle = 'DMY';
 copy sales_data FROM 'C:\Analytics Engineer\SQL PROJECTS\retail_analysis\csv_file\superstore_sale.csv' WITH (FORMAT csv, HEADER true, DELIMITER ';');
 
-SELECT * FROM sales_data LIMIT 10;
+SELECT * FROM sales_data;
